c1d3e94a356ec0e90f3d906be01d243a97bb5f13
Saull Brandão <saull@outlook.com>
2021-06-28 10:52:38 -0300

    fix: copy icon size was too small in small screens

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -14,10 +14,10 @@ export function RoomCode({ code }: RoomCodeProps) {
       onClick={copyRoomCodeToClipboard}
       className='h-10 rounded-lg overflow-hidden bg-white border border-purple cursor-pointer flex'
     >
-      <div className='bg-purple h-full px-3 flex justify-center items-center'>
+      <div className='bg-purple h-full px-1 sm:px-2 flex justify-center items-center'>
         <img src={copyImg} alt="Copy room code" />
       </div>
-      <span className='self-center flex-1 px-2 max-w-40 text-sm font-medium block sm:hidden whitespace-nowrap'>
+      <span className='self-center flex-1 px-1 max-w-40 text-sm font-medium block sm:hidden whitespace-nowrap'>
         Room code
       </span>
       <span className='self-center flex-1 pl-3 pr-4 w-64 text-sm font-medium hidden sm:block'>
